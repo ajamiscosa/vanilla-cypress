b1789b8e111498d6521726bfe460f68c4c93f6c5
Aron Jhed Amiscosa <ajamiscosa@gmail.com>
2023-05-09 10:06:46 +0800

    Add cypress-real-events support

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'q2ybs6',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     viewportWidth: 1920,
-    viewportHeight: 1080
+    viewportHeight: 1080,
+    watchForFileChanges: false
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,31 +1,35 @@
 describe('Basic', () => {
-  it('login', () => {
-    cy.visit('http://localhost:8180/pc/PolicyCenter.do');
-    cy.get('#Login-LoginScreen-LoginDV-username').type('su');
-    cy.get('#Login-LoginScreen-LoginDV-password').type('gw');
-    cy.get('#Login-LoginScreen-LoginDV-submit').click();
-  });
+    it('login', () => {
+        cy.visit('http://localhost:8180/pc/PolicyCenter.do');
+        cy.get('#Login-LoginScreen-LoginDV-username').type('su');
+        cy.get('#Login-LoginScreen-LoginDV-password').type('gw');
+        cy.get('#Login-LoginScreen-LoginDV-submit').click();
+    });
 
-  it('create account', () => {
-    cy.visit('http://localhost:8180/pc/PolicyCenter.do');
-    cy.get('#Login-LoginScreen-LoginDV-username').type('su');
-    cy.get('#Login-LoginScreen-LoginDV-password').type('gw');
-    cy.get('#Login-LoginScreen-LoginDV-submit').click();
-    
-    cy.get('#TabBar-AccountTab').click();
-    //   .find('.gw-action--expand-button')
-    //   .click();
+    it('create account', () => {
+        cy.visit('http://localhost:8180/pc/PolicyCenter.do');
+        cy.get('#Login-LoginScreen-LoginDV-username').type('su');
+        cy.get('#Login-LoginScreen-LoginDV-password').type('gw');
+        cy.get('#Login-LoginScreen-LoginDV-submit').click();
 
-    // cy.get('#TabBar-AccountTab-AccountTab_NewAccount').click();
-    // cy.get('#NewAccount-NewAccountScreen-ttlBar').should('contain.text', 'Enter Account Information');
+        cy.get('#TabBar-AccountTab').click();
+        // .find('.gw-action--expand-button')
+        // .click();
 
-    // cy.findByLabelText("First name").type('John');
-    // cy.findByLabelText("Last name").type('Smith');
-    // cy.get('#NewAccount-NewAccountScreen-NewAccountSearchDV-SearchAndResetInputSet-SearchLinksInputSet-Search').click();
+        // cy.get('#TabBar-AccountTab-AccountTab_NewAccount').click();
+        // cy.get('#NewAccount-NewAccountScreen-ttlBar').should('contain.text', 'Enter Account Information');
 
-    // cy.get('#NewAccount-NewAccountScreen-NewAccountButton').click().get('#NewAccount-NewAccountScreen-NewAccountButton-NewAccount_Person').click();
+        // cy.findByLabelText("First name").type('John');
+        // cy.findByLabelText("Last name").type('Smith');
+        // cy.get('#NewAccount-NewAccountScreen-NewAccountSearchDV-SearchAndResetInputSet-SearchLinksInputSet-Search').click();
 
-    // cy.readFile('cypress/e2e/testdata/account.json').then((json) => {
+        //
+        // cy.get('#NewAccount-NewAccountScreen-NewAccountButton')
+        //     .click()
+        //     .get('#NewAccount-NewAccountScreen-NewAccountButton-NewAccount_Person')
+        //         .click();
+
+        // cy.readFile('cypress/e2e/testdata/account.json').then((json) => {
         // cy.findByLabelText('Home Phone').type(json.homePhone);
         // cy.focused().tab();
         // cy.findByLabelText("Primary Email").type(json.primaryEmail);
@@ -38,9 +42,23 @@ describe('Basic', () => {
         // cy.get('div[id$=-SelectOrganization').click();
         // cy.findByLabelText("Producer Code").find('select').select(json.producerCode);
         // cy.findByLabelText("Update").click();
-    // });
+        // });
 
-    cy.get('[aria-label=Actions][role=button]').click();
-    cy.get('div[id$=-AccountFileMenuActions_NewSubmission').click();
-  });
+        cy.get('[aria-label=Actions][role=button]').click();
+        cy.get('div[id$=-AccountFileMenuActions_NewSubmission').click();
+
+        cy.get('div[id$=-ProductSelectionLV]')
+            .contains('td', 'Personal Auto')
+            .siblings()
+            .find('div[id$="-addSubmission"]')
+            .click();
+
+        cy.findByLabelText('Offering Selection').find('select').select('Standard Program');
+        cy.findByLabelText("Next").click();
+        cy.findByLabelText("Next").click();
+        cy.findByLabelText("Next").click();
+        cy.get('div[id$=-AddDriver]').click();
+        cy.get('div[id$=-AddExistingContact] > [aria-haspopup="true"]').realHover('mouse');
+        cy.get('div[id$=-AddExistingContact-0-UnassignedDriver]').click();
+    });
 });
